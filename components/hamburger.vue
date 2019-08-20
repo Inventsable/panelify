582c35eb79ff08bf30b81694e7a7d0c0c7cfdf36
Tom Scharstein <trscharstein@gmail.com>
2019-08-19 18:31:29 -0700

    Finish dynamic themes w/ starlette

--- components/hamburger.vue
+++ components/hamburger.vue
@@ -1,15 +1,20 @@
 <template>
   <svg viewBox="0 0 24 24">
-    <path class="menu-fill" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
+    <path :style="getMenuStyle()" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" />
   </svg>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    color: String,
+  },
+  methods: {
+    getMenuStyle() {
+      return `
+        fill: ${this.color};
+      `
+    }
+  }
+};
 </script>
-
-<style>
-.menu-fill {
-  fill: var(--color-btn-pill-border);
-}
-</style>
--- components/panel.vue
+++ components/panel.vue
@@ -1,22 +1,21 @@
 <template>
-  <!-- <div class="fake-panel-bg"> -->
-  <div class="fake-panel-wrapper">
-    <div class="fake-panel-header">
-      <div class="fake-panel-tab">
-        <div class="fake-panel-title">{{extName}}</div>
-        <div class="fake-panel-menu">
-          <hamburger />
+  <div class="block-panel-wrapper" :style="getWrapperStyle()">
+    <div class="block-panel-header" :style="getHeaderStyle()">
+      <div class="block-panel-tab" :style="getTabStyle()">
+        <div class="block-panel-title">{{extName}}</div>
+        <div class="block-panel-menu">
+          <hamburger :color="getMenuColor()" />
         </div>
       </div>
     </div>
-    <div class="fake-panel-content">
-      <slot></slot>
+    <div class="block-panel-content">
+      <iframe :src="this.src" frameborder="0" style="width: 100%; height: 100%;"></iframe>
     </div>
   </div>
-  <!-- </div> -->
 </template>
 
 <script>
+import starlette from "starlette";
 import hamburger from "./hamburger.vue";
 
 export default {
@@ -25,16 +24,84 @@ export default {
     extName: String,
     appName: String,
     theme: String,
-    src: String,
+    width: String,
+    height: String,
+    gradient: Number,
+    src: String
+  },
+  computed: {
+    isBlock() {
+      return /aeft|ppro|audt/i.test(this.appName);
+    }
   },
   components: {
     hamburger
+  },
+  mounted() {
+    // starlette.initAs(this.appName, this.theme);
+  },
+  methods: {
+    getWrapperStyle() {
+      return `
+        width: ${this.width};
+        height: ${this.height};
+        border: 4px solid ${starlette.getColorAs(
+          "color-header-border",
+          this.appName,
+          this.theme,
+          this.gradient || null
+        )};
+        background-color: ${starlette.getColorAs(
+          "color-bg",
+          this.appName,
+          this.theme,
+          this.gradient || null
+        )};
+        color: ${starlette.getColorAs(
+          "color-btn-pill-border",
+          this.appName,
+          this.theme,
+          this.gradient || null
+        )};
+      `;
+    },
+    getHeaderStyle() {
+      return `
+        width: 100%;
+        border-color: ${starlette.getColorAs(
+          "color-scrollbar-thumb",
+          this.appName,
+          this.theme,
+          this.gradient || null
+        )};
+      `;
+    },
+    getTabStyle() {
+      return `
+        border-color: ${starlette.getColorAs(
+          "color-btn-pill-border",
+          this.appName,
+          this.theme,
+          this.gradient || null
+        )};
+      `;
+    },
+    getMenuColor() {
+      return `${starlette.getColorAs(
+        "color-btn-pill-border",
+        this.appName,
+        this.theme,
+        this.gradient || null
+      )}`;
+    }
   }
 };
 </script>
 
 <style>
-.fake-panel-bg {
+@import url(//fonts.googleapis.com/css?family=Open+Sans);
+
+.block-panel-bg {
   background-color: #fff;
 
   flex-direction: column;
@@ -42,60 +109,48 @@ export default {
   align-items: center;
 }
 
-.fake-panel-wrapper {
+.block-panel-wrapper {
   box-sizing: border-box;
-  width: 400px;
-  height: 400px;
-  border: 4px solid #161616;
-  background-color: var(--color-bg);
-  position: absolute;
   display: flex;
   flex-wrap: wrap;
   justify-content: flex-start;
   align-items: center;
-  top: 0px;
-  left: 0px;
 }
 
-.fake-panel-title {
+.block-panel-title {
   font-family: "Open Sans";
   font-size: 12px;
   user-select: none;
   cursor: default;
-  color: var(--color-btn-pill-border);
 }
 
-.fake-panel-tab {
+.block-panel-tab {
   box-sizing: border-box;
   display: flex;
   justify-content: flex-start;
   align-items: center;
   border-style: solid;
   border-width: 0px 0px 2px 0px;
-  border-color: var(--color-btn-pill-border);
 }
 
-.fake-panel-menu {
+.block-panel-menu {
   padding-left: 8px;
   padding-top: 6px;
   width: 14px;
 }
 
-.fake-panel-header {
-  color: var(--color-default);
+.block-panel-header {
   align-items: center;
-  width: auto;
+  width: 100%;
   padding-left: 10px;
   display: flex;
   flex-wrap: nowrap;
   box-sizing: border-box;
   height: 30px;
-  background-color: #232323;
   border-style: solid;
-  border-width: 0px 0px 2px 0px;
-  border-color: #292929;
+  border-width: 0px 0px 1px 0px;
 }
-.fake-panel-content {
+.block-panel-content {
   box-sizing: border-box;
   height: calc(100% - 30px);
   width: 100%;
