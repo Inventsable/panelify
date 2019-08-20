7e2b09f740b2f3b734af5890bd1cf09b0d5d92dc
Tom Scharstein <trscharstein@gmail.com>
2019-08-19 22:19:57 -0700

    Publish npm package, AEFT full support

--- components/panel.vue
+++ components/panel.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="block-panel-wrapper" :style="getWrapperStyle()">
-    <div class="block-panel-header" :style="getHeaderStyle()">
-      <div class="block-panel-tab" :style="getTabStyle()">
-        <div class="block-panel-title">{{extName}}</div>
-        <div class="block-panel-menu">
+  <div class="panelify-wrapper">
+    <div class="panelify-controller">
+      <div
+        class="panelify-controller-button"
+        v-for="(col, i) in themeBlocks"
+        :key="i"
+        @click="changeActiveBlock(col)"
+        :style="getControllerButtonStyle(i, col)"
+      ></div>
+    </div>
+    <div class="block-panel-wrapper" :style="getWrapperStyle()">
+      <div class="block-panel-header" :style="getHeaderStyle()">
+        <div class="block-panel-tab" :style="getTabStyle()">
+          <div class="block-panel-title">{{extName}}</div>
+          <div v-if="isBlock" class="block-panel-menu">
+            <hamburger :color="getMenuColor()" />
+          </div>
+        </div>
+        <div v-if="!isBlock" style="padding-right: 10px;" class="block-panel-menu">
           <hamburger :color="getMenuColor()" />
         </div>
       </div>
-    </div>
-    <div class="block-panel-content">
-      <iframe :src="this.src" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+      <div class="block-panel-content">
+        <iframe ref="panel" :src="this.src" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+      </div>
     </div>
   </div>
 </template>
@@ -32,36 +46,137 @@ export default {
   computed: {
     isBlock() {
       return /aeft|ppro|audt/i.test(this.appName);
+    },
+    themeBlocks() {
+      return this.isBlock ? this.AEFTcontrollers : this.ILSTcontrollers;
+    },
+    activeController() {
+      return this.themeBlocks.find(controller => {
+        return controller.active;
+      });
     }
   },
+  watch: {
+    activeController(item) {
+      this.updatePanelifyTheme();
+    }
+  },
+  data: () => ({
+    newTheme: null,
+    newGradient: null,
+    AEFTcontrollers: [
+      {
+        active: false,
+        value: 0,
+        panelBG: [35, 35, 35]
+      },
+      {
+        active: false,
+        value: 50,
+        panelBG: [59, 59, 59]
+      },
+      {
+        active: false,
+        value: 100,
+        panelBG: [83, 83, 83]
+      }
+    ],
+    ILSTcontrollers: [
+      {
+        active: false,
+        value: "darkest",
+        panelBG: [50, 50, 50]
+      },
+      {
+        active: false,
+        value: "dark",
+        panelBG: [83, 83, 83]
+      },
+      {
+        active: false,
+        value: "light",
+        panelBG: [184, 184, 184]
+      },
+      {
+        active: false,
+        value: "lightest",
+        panelBG: [240, 240, 240]
+      }
+    ]
+  }),
   components: {
     hamburger
   },
   mounted() {
     // starlette.initAs(this.appName, this.theme);
+    let themeController;
+    themeController = this.isBlock
+      ? this.AEFTcontrollers.find(item => {
+          return item.value == this.gradient;
+        })
+      : this.ILSTcontrollers.find(item => {
+          return item.value == this.theme;
+        });
+    themeController.active = true;
+    const self = this;
+    if (this.src) this.$refs.panel.onload = () => {
+        self.updatePanelifyTheme();
+      };
   },
   methods: {
+    updatePanelifyTheme() {
+      let msg = this.isBlock
+        ? `${this.appName};${this.activeController.value}`
+        : `${this.appName}${this.activeController.value}`;
+      if (this.src) this.$refs.panel.contentWindow.postMessage(msg, "*");
+    },
+    clearActives() {
+      if (this.isBlock) {
+        this.AEFTcontrollers.forEach(controller => {
+          controller.active = false;
+        });
+      } else {
+        this.ILSTcontrollers.forEach(controller => {
+          controller.active = false;
+        });
+      }
+    },
+    changeActiveBlock(item) {
+      this.clearActives();
+      item.active = true;
+      if (this.isBlock) {
+        this.newGradient = item.value;
+      } else {
+        this.newTheme = item.value;
+      }
+    },
     getWrapperStyle() {
       return `
         width: ${this.width};
         height: ${this.height};
-        border: 4px solid ${starlette.getColorAs(
-          "color-header-border",
-          this.appName,
-          this.theme,
-          this.gradient || null
-        )};
+        border: ${this.isBlock ? "4px" : "2px"} solid ${starlette.getColorAs(
+        "color-header-border",
+        this.appName,
+        this.newTheme || this.theme,
+        this.newGradient || this.newGradient == 0
+          ? this.newGradient
+          : this.gradient || null
+      )};
         background-color: ${starlette.getColorAs(
           "color-bg",
           this.appName,
-          this.theme,
-          this.gradient || null
+          this.newTheme || this.theme,
+          this.newGradient || this.newGradient == 0
+            ? this.newGradient
+            : this.gradient || null
         )};
         color: ${starlette.getColorAs(
           "color-btn-pill-border",
           this.appName,
-          this.theme,
-          this.gradient || null
+          this.newTheme || this.theme,
+          this.newGradient || this.newGradient == 0
+            ? this.newGradient
+            : this.gradient || null
         )};
       `;
     },
@@ -71,27 +186,88 @@ export default {
         border-color: ${starlette.getColorAs(
           "color-scrollbar-thumb",
           this.appName,
-          this.theme,
-          this.gradient || null
+          this.newTheme || this.theme,
+          this.newGradient || this.newGradient == 0
+            ? this.newGradient
+            : this.gradient || null
         )};
+        border-width: ${this.isBlock ? "0px 0px 1px 0px" : "0px"};
+        background-color: ${
+          !this.isBlock
+            ? starlette.getColorAs(
+                "color-header",
+                this.appName,
+                this.newTheme || this.theme
+              )
+            : "transparent"
+        };
       `;
     },
     getTabStyle() {
       return `
+        border-width: ${this.isBlock ? "0px 0px 2px 0px" : "0px"};
         border-color: ${starlette.getColorAs(
           "color-btn-pill-border",
           this.appName,
-          this.theme,
-          this.gradient || null
+          this.newTheme || this.theme,
+          this.newGradient || this.newGradient == 0
+            ? this.newGradient
+            : this.gradient || null
         )};
+        background-color: ${
+          !this.isBlock
+            ? starlette.getColorAs(
+                "color-bg",
+                this.appName,
+                this.newTheme || this.theme
+              )
+            : "transparent"
+        };
+        padding: ${this.isBlock ? "0px" : "0px 10px"};
+        margin-left: ${this.isBlock ? "10px" : "0px"};
+      `;
+    },
+    getControllerButtonStyle(index, color) {
+      let theme, gradient;
+      if (!this.isBlock) {
+        theme = color.value;
+      } else {
+        theme = "gradient";
+        gradient = index * 50;
+        // console.log(`Value of ${index} is ${gradient}`);
+      }
+      return `
+        background-color: ${starlette.getColorAs(
+          "color-bg",
+          this.appName,
+          theme,
+          gradient || null
+        )};
+        border-color: ${
+          color.active
+            ? starlette.getColorAs(
+                "color-selection",
+                this.appName,
+                theme,
+                gradient || null
+              )
+            : starlette.getColorAs(
+                "color-header-border",
+                this.appName,
+                theme,
+                gradient || null
+              )
+        };
       `;
     },
     getMenuColor() {
       return `${starlette.getColorAs(
         "color-btn-pill-border",
         this.appName,
-        this.theme,
-        this.gradient || null
+        this.newTheme || this.theme,
+        this.newGradient || this.newGradient == 0
+          ? this.newGradient
+          : this.gradient || null
       )}`;
     }
   }
@@ -100,6 +276,18 @@ export default {
 
 <style>
 @import url(//fonts.googleapis.com/css?family=Open+Sans);
+
+.panelify-wrapper {
+  display: flex;
+}
+
+.panelify-controller-button {
+  width: 20px;
+  height: 20px;
+  border-width: 2px;
+  border-style: solid;
+  cursor: pointer;
+}
 
 .block-panel-bg {
   background-color: #fff;
@@ -130,7 +318,7 @@ export default {
   justify-content: flex-start;
   align-items: center;
   border-style: solid;
-  border-width: 0px 0px 2px 0px;
+  height: 100%;
 }
 
 .block-panel-menu {
@@ -142,13 +330,12 @@ export default {
 .block-panel-header {
   align-items: center;
   width: 100%;
-  padding-left: 10px;
   display: flex;
+  justify-content: space-between;
   flex-wrap: nowrap;
   box-sizing: border-box;
   height: 30px;
   border-style: solid;
-  border-width: 0px 0px 1px 0px;
 }
 .block-panel-content {
   box-sizing: border-box;
